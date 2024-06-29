8c8fa0a06c48f20c745b37b8325518a3d3efade1
nezhataghy <nezhataghy@gmail.com>
2024-06-29 14:52:18 +0000

    Establish MongoDB connection in Express server

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express")
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 
 const app = express()
 
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to the DB");
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+    });
+})
+.catch((err) => {
+    console.log({message: err.message})
+})
