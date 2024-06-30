c60926d2496ef3724e28b6d8a753bef78e6a5d11
nezhataghy <nezhataghy@gmail.com>
2024-06-30 16:40:36 +0000

    Setup Express server with MongoDB connection, middleware, and routes

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require("express")
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+const authenticate = require('./middleware/authenticate');
 
 const app = express()
-
-
-
-
-
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("Connected to the DB");
-    app.listen(5000, () => {
-        console.log('Server is running on port 5000');
-    });
 })
 .catch((err) => {
     console.log({message: err.message})
 })
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/users', require('./routes/user'));
+app.use('/auth', require('./routes/auth'));
+app.use('/sensors', authenticate, require('./routes/sensor'));
+app.use('/actuators', authenticate, require('./routes/actuator'));
+
+app.get("/", (req, res) => {
+    res.send("SmartHome Project!")
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
