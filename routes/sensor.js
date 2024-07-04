97206b88e9637d8b4bb378f7895e23f81ffa210a
nezhataghy <nezhataghy@gmail.com>
2024-07-04 16:51:19 +0000

    Eliminate unnecessary lines

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sensorsController = require('../controllers/SensorsController');
-const authenticate = require('../middleware/authenticate');
 
-router.use(authenticate);
-
-// Define the routes and map them to the controller functions
 router.get('/', sensorsController.getSensorsReading);
 router.post('/', sensorsController.createSensorsReading);
 router.get('/:id', sensorsController.getSensorById);
