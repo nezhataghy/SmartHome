8617a05978a59f0d42dfeb0a68966e4e0c288a6a
nezhataghy <nezhataghy@gmail.com>
2024-07-04 16:47:51 +0000

    Error handling - UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -16,25 +16,26 @@ exports.getUsers = async (req, res) => {
     }
   };
 
-exports.getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.sendStatus(404);
-    res.json(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
+  exports.getUserById = async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
 
-exports.updateUser = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!user) return res.sendStatus(404);
-    res.json(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
+  exports.updateUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
+  
 
 exports.deleteUser = async (req, res) => {
     try {
