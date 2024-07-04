b26aebf067fe21bba11ea2918a48e82114e154a9
nezhataghy <nezhataghy@gmail.com>
2024-07-04 12:44:47 +0000

    Establish WebSocket server for real-time actuator control

---
+++ websocketServer.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+module.exports = wss;
