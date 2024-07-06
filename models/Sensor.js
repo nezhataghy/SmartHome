e3f55b2a0edf2e6c68593ef43dc861015f88c8c3
nezhataghy <nezhataghy@gmail.com>
2024-07-06 01:12:12 +0000

    Sensor schema file formatted

--- models/Sensor.js
+++ models/Sensor.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const sensorReadingSchema = new mongoose.Schema({
-  temperature: { type: Number, required: true },
-  humidity: { type: Number, required: true },
-  flameValue: { type: Number, required: true },
+  temperature: { 
+    type: Number, 
+    required: true 
+  },
+  humidity: {
+    type: Number, 
+    required: true 
+  },
+  flameValue: { 
+    type: Number, 
+    required: true 
+  },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-
 }, { timestamps: true });
 
 module.exports = mongoose.model('SensorsReading', sensorReadingSchema);
